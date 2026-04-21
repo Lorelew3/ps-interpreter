0a5edcfc5c79f22a122400a543ad1fe9c5ba6277
Lorelew3 <lorelew3@gmail.com>
2026-04-21 06:05:58 -0700

    working interpreter passes tests

---
+++ parser.go
@@ -0,0 +1,100 @@
+package main
+
+// execution engine
+func (ip *Interpreter) execute(tokens []string) {
+	for i := 0; i < len(tokens); i++ {
+		t := tokens[i]
+
+		switch t {
+
+		case "add":
+			ip.Add()
+		case "sub":
+			ip.Sub()
+		case "mul":
+			ip.Mul()
+		case "div":
+			ip.Div()
+		case "mod":
+			ip.Mod()
+		case "abs":
+			ip.Abs()
+		case "neg":
+			ip.Neg()
+
+		case "floor":
+			ip.Floor()
+		case "ceiling":
+			ip.Ceiling()
+		case "round":
+			ip.Round()
+		case "sqrt":
+			ip.Sqrt()
+		case "idiv":
+			ip.Idiv()
+		case "copy":
+			ip.Copy()
+
+		case "dup":
+			ip.Dup()
+		case "exch":
+			ip.Exch()
+		case "pop":
+			ip.PopOp()
+		case "clear":
+			ip.Clear()
+		case "count":
+			ip.Count()
+
+		case "eq":
+			ip.Eq()
+		case "ne":
+			ip.Ne()
+		case "lt":
+			ip.Lt()
+		case "gt":
+			ip.Gt()
+		case "ge":
+			ip.Ge()
+		case "le":
+			ip.Le()
+
+		case "and":
+			ip.And()
+		case "or":
+			ip.Or()
+		case "not":
+			ip.Not()
+		case "true":
+			ip.True()
+		case "false":
+			ip.False()
+
+		case "dict":
+			ip.Dict()
+		case "begin":
+			ip.BeginDict()
+		case "end":
+			ip.EndDict()
+		case "def":
+			ip.DefOp()
+
+		case "=":
+			ip.PrintEq()
+		case "print":
+			ip.Print()
+		case "==":
+			ip.PrintPP()
+
+		default:
+			if val, ok := ip.Lookup(t); ok {
+				switch v := val.(type) {
+				case Procedure:
+					ip.Call(v)
+				default:
+					ip.Push(v)
+				}
+			}
+		}
+	}
+}
--- stdlib.go
+++ stdlib.go
@@ -1,9 +1,12 @@
 package main
 
-// for I/O
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// stack ops
+// ===================== STACK OPS =====================
+
 func (ip *Interpreter) Dup() {
 	v := ip.Pop()
 	ip.Push(v)
@@ -29,27 +32,155 @@ func (ip *Interpreter) Count() {
 	ip.Push(len(ip.stack))
 }
 
-// arithmetic (full set)
-func (ip *Interpreter) Add() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a + b) }
-func (ip *Interpreter) Sub() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a - b) }
-func (ip *Interpreter) Mul() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a * b) }
-func (ip *Interpreter) Div() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a / b) }
-func (ip *Interpreter) Mod() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a % b) }
+func (ip *Interpreter) Copy() {
+	n := ip.Pop().(int)
+	if n > len(ip.stack) {
+		panic("stack underflow")
+	}
 
-// comparisons
-func (ip *Interpreter) Eq() { b := ip.Pop(); a := ip.Pop(); ip.Push(a == b) }
-func (ip *Interpreter) Ne() { b := ip.Pop(); a := ip.Pop(); ip.Push(a != b) }
-func (ip *Interpreter) Lt() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a < b) }
-func (ip *Interpreter) Gt() { b := ip.Pop().(int); a := ip.Pop().(int); ip.Push(a > b) }
+	start := len(ip.stack) - n
+	copied := make([]interface{}, n)
+	copy(copied, ip.stack[start:])
+	ip.stack = append(ip.stack, copied...)
+}
 
-// boolean
-func (ip *Interpreter) And() { b := ip.Pop().(bool); a := ip.Pop().(bool); ip.Push(a && b) }
-func (ip *Interpreter) Or()  { b := ip.Pop().(bool); a := ip.Pop().(bool); ip.Push(a || b) }
-func (ip *Interpreter) Not() { a := ip.Pop().(bool); ip.Push(!a) }
+// ===================== ARITHMETIC =====================
 
-// dictionary ops
+func (ip *Interpreter) Add() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a + b)
+}
+
+func (ip *Interpreter) Sub() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a - b)
+}
+
+func (ip *Interpreter) Mul() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a * b)
+}
+
+func (ip *Interpreter) Div() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a / b)
+}
+
+func (ip *Interpreter) Mod() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a % b)
+}
+
+func (ip *Interpreter) Abs() {
+	a := ip.Pop().(int)
+	if a < 0 {
+		a = -a
+	}
+	ip.Push(a)
+}
+
+func (ip *Interpreter) Neg() {
+	a := ip.Pop().(int)
+	ip.Push(-a)
+}
+
+func (ip *Interpreter) Floor() {
+	ip.Push(ip.Pop().(int))
+}
+
+func (ip *Interpreter) Ceiling() {
+	ip.Push(ip.Pop().(int))
+}
+
+func (ip *Interpreter) Round() {
+	ip.Push(ip.Pop().(int))
+}
+
+func (ip *Interpreter) Sqrt() {
+	a := ip.Pop().(int)
+	ip.Push(int(math.Sqrt(float64(a))))
+}
+
+func (ip *Interpreter) Idiv() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a / b)
+}
+
+// ===================== COMPARISONS =====================
+
+func (ip *Interpreter) Eq() {
+	b := ip.Pop()
+	a := ip.Pop()
+	ip.Push(a == b)
+}
+
+func (ip *Interpreter) Ne() {
+	b := ip.Pop()
+	a := ip.Pop()
+	ip.Push(a != b)
+}
+
+func (ip *Interpreter) Lt() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a < b)
+}
+
+func (ip *Interpreter) Gt() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a > b)
+}
+
+func (ip *Interpreter) Ge() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a >= b)
+}
+
+func (ip *Interpreter) Le() {
+	b := ip.Pop().(int)
+	a := ip.Pop().(int)
+	ip.Push(a <= b)
+}
+
+// ===================== BOOLEAN =====================
+
+func (ip *Interpreter) And() {
+	b := ip.Pop().(bool)
+	a := ip.Pop().(bool)
+	ip.Push(a && b)
+}
+
+func (ip *Interpreter) Or() {
+	b := ip.Pop().(bool)
+	a := ip.Pop().(bool)
+	ip.Push(a || b)
+}
+
+func (ip *Interpreter) Not() {
+	a := ip.Pop().(bool)
+	ip.Push(!a)
+}
+
+func (ip *Interpreter) True() {
+	ip.Push(true)
+}
+
+func (ip *Interpreter) False() {
+	ip.Push(false)
+}
+
+// ===================== DICTIONARY =====================
+
 func (ip *Interpreter) Dict() {
-	ip.Pop()
+	ip.Pop() // ignore size
 	ip.Push(make(map[string]interface{}))
 }
 
@@ -68,12 +199,40 @@ func (ip *Interpreter) DefOp() {
 	ip.Def(name, val)
 }
 
-// I/O
+// ===================== OUTPUT =====================
+
 func (ip *Interpreter) PrintEq() {
 	fmt.Println(ip.Pop())
 }
 
-// scoping
+func (ip *Interpreter) Print() {
+	fmt.Print(ip.Pop())
+}
+
+func (ip *Interpreter) PrintPP() {
+	fmt.Printf("%#v\n", ip.Pop())
+}
+
+// ===================== FLOW CONTROL =====================
+
+func (ip *Interpreter) If(proc Procedure) {
+	cond := ip.Pop().(bool)
+	if cond {
+		ip.Call(proc)
+	}
+}
+
+func (ip *Interpreter) IfElse(p1, p2 Procedure) {
+	cond := ip.Pop().(bool)
+	if cond {
+		ip.Call(p1)
+	} else {
+		ip.Call(p2)
+	}
+}
+
+// ===================== SCOPING =====================
+
 func (ip *Interpreter) Call(proc Procedure) {
 	if ip.lexical {
 		old := ip.dictStack
